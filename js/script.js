138d26b3a7ad7b23dbeeaf60ef4c0861b4bf6f12
Yuri Faria <yuri.yurifaria@gmail.com>
2019-07-07 00:33:31 -0300

    Big mudances

--- js/script.js
+++ js/script.js
@@ -1,59 +1,19 @@
-function getGithubData() {
-  return new Promise(async (next, reject) => {
-  	try {
-  	  const call = await fetch(`https://api.github.com/users/Windows87`);
-  	  const callRepos = await fetch(`https://api.github.com/users/Windows87/repos`);
+function setTerminalStyle(name, element, end = 0) {
+  setTimeout(() => {
+    if(name.length === end) {
+      element.innerText = name;
 
-  	  const res = await call.json();
-  	  const resRepos = await callRepos.json();
-
-  	  res.repos = resRepos;
-
-  	  next(res);
-    } catch(error) {
-      reject(error);
+    } else {
+      let show = `${name.substring(0, end)}|`;
+      element.innerText = show;
+      setTerminalStyle(name, element, ++end);
     }
-  });
+  }, 100);
 }
 
-function setReposData(githubData) {
-  const reposLanguages = [];
-  
-  const repoNumberElement = document.querySelector('#repos');
-  const reposLanguagesElement = document.querySelector('#repos-languages');
-
-  repoNumberElement.innerText = `${githubData.public_repos} Repositórios Públicos`;
-
-  githubData.repos.forEach(repo => {
-  	if(reposLanguages[repo.language])
-  	  reposLanguages[repo.language]++;
-  	else
-  	  reposLanguages[repo.language] = 1;
-  });
-
-  console.log(reposLanguages);
-
-  for(let i = 1; i < Object.getOwnPropertyNames(reposLanguages).length; i++) {
-  	const repoLanguageElement = document.createElement('span');
-  	const lang = Object.getOwnPropertyNames(reposLanguages)[i]; 
-
-  	if(reposLanguages[lang] > 1)
-  	  repoLanguageElement.innerText = `${reposLanguages[lang]} repositórios ${lang}`;
-  	else
-  	  repoLanguageElement.innerText = `${reposLanguages[lang]} repositório ${lang}`;
-
-  	reposLanguagesElement.appendChild(repoLanguageElement);
-  }
-
+function start() {
+  setTerminalStyle('Yuri Faria', document.querySelector('h1'));
+  setTerminalStyle('Full Stack Developer', document.querySelector('h2'));
 }
 
-async function setGithubData() {
-  try {
-  	const githubData = await getGithubData();
-  	setReposData(githubData);
-  } catch(error) {
-  	console.log(error);
-  }
-}
-
-setGithubData();
+start();
